3e0cb592e68f13aa709325a966f6eeaaa32595ee
Guillermo De La Rosa Orea <87319828+gdelarosa7@users.noreply.github.com>
2022-08-05 12:11:14 -0500

    Update EulerMejorado4.py

--- EulerMejorado4.py
+++ EulerMejorado4.py
@@ -22,3 +22,57 @@ xf = float(input('Da valor final de x: '))
 y =  float(input('Da valor inicial de y: '))
 # leemos n, para que quede más claro lo manejamos 
 # como subintervalos o numero de iteraciones:
+it = int(input('Da el número de subintervalos(iteraciones): '))
+
+#inicializamos los vectores para registrar los valores x,y (xV,yV)
+# llenamos el vector con ceros
+xV = np.zeros(it+1)
+# valor inicial x
+xV[0] = x 
+
+yV = np.zeros(it+1)
+# valor inicial de y 
+yV[0] = y
+
+# Algoritmo tomado del libro:
+# Métodos numéricos aplicados a la ingeniería, pag 549 
+
+# Función en donde insertamos la ED a resolver
+def f(x,y):
+  return (x-y)
+
+# Paso 1
+# Calculamos el valor de h
+h= (xf - x)/it
+# Calculamos los valores de x+h
+for i in range(it):
+  xV[i+1] = xV[i] + h
+  
+# Paso 2 Y 3 
+for i in range(0,it):
+    # Paso 4 PREDICTOR valor temporal 
+    ft = yV[i] + h*(f(xV[i],yV[i]))
+    # Paso 5 CORRECTOR 
+    yV[i+1] = yV[i] + h*((f(xV[i],yV[i]) + f(xV[i+1],ft))/ 2 )
+    # Paso 6 y 7 - el incremento x+h ya esta en el vector x,
+    # solamente tomamos el valor para la iteración siguiente
+print() 
+   
+# paso 8 imprimir resultado
+print('Resultado:')
+#print(xV)
+# Creamos una lista con el arreglo yV
+nlist = list()
+for i in yV:
+  nlist.append(i)
+for i in nlist:
+  print(i)
+#print(yV)
+
+# Gráfica
+plt.scatter(xV,yV).set_color("blue")
+plt.title('ED - Método de Euler mejorado')
+plt.xlabel('Intervalo x')
+plt.ylabel('y')
+# plt.grid() 
+plt.show()
